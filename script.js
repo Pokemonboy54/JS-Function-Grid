4c7c65ccdb77e55300a38f14b2319c6cd3f79559
Sammy Ech-Chaouy <sech-chaouy@aclanyc.com>
2017-02-27 13:19:08 +0000

    hi

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//Write your function calls below this line
+
+setColor(0, 0, "blue");
+setColor(9, 0, "blue");
+setColor(9, 9, "blue");
+setColor(0, 9, "blue");
+
+//red is smile
+setColor(2, 4, "red");
+setColor(2, 5, "red");
+setColor(5, 4, "red");
+setColor(5, 5, "red");
+setColor(4, 3, "red");
+setColor(4, 6, "red");
